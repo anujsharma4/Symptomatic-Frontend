84b9e7e372a0d99c0fbe730ede069bb634c84c93
github username <github email address>
2019-02-22 14:40:25 -0500

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// import { Header } from 'semantic-ui-react'
+import logo from '../images/logo.png'
+
+
+const Home = (props) => {
+  return(
+    <div className="homepic">
+      <img className="homepic" alt="logo" src={logo}/>
+    </div>
+  )
+}
+
+
+export default Home
